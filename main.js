535feef6411efd8cf693e899bfbfd190a01421cd
JJ-Wph <kuba.wypych92@gmail.com>
2022-07-08 16:45:29 +0200

    CSS styling updated, creating new buttons

--- main.js
+++ main.js
@@ -3,33 +3,33 @@ new Vue({
     data:{
       nickName: "",
       password: "",
-      modalContent: "Zaloguj się!",
+      formHeader: "Zaloguj się!",
       isLoggin: false,
       playerData: {
-        nick: "holender",
+        nick: "admin",
         password: "qwerty",
       }
     },
 
     methods:{
       logginIn: function(){
-        this.modalContent = "Logowanie..."
+        this.formHeader = "Logowanie..."
         if(this.password === this.playerData.password && this.nickName === this.playerData.nick){
           var that = this;
           setTimeout(function(){
-            that.modalContent = "Udało się! Logowanie udane!";
+            that.formHeader = "Udało się! Logowanie udane!";
           }, 2000);
           setTimeout(function(){
-            that.modalContent = "";
+            that.formHeader = "";
             that.isLoggin = true;
           }, 4000);
         } else {
           var that = this;
           setTimeout(function(){
-            that.modalContent = "Błąd logowania! Błędny nick lub hasło";
+            that.formHeader = "Błąd logowania! Błędny nick lub hasło";
           }, 2000);
           setTimeout(function(){
-            that.modalContent = "Zaloguj się ponownie!";
+            that.formHeader = "Zaloguj się ponownie!";
             that.nickName = "";
             that.password = "";
           }, 4000);
